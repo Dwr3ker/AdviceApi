2a64d0b81315b0fd1b691ce314ae186c6911a916
Glitch (8-1-api-project) <none>
2020-05-18 01:07:24 +0000

    🌘😅 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,34 @@
+let searchButton = document.querySelector("#search")
+
+//Add an event listener to the button that runs the function sendApiRequest when it is clicked
+searchButton.addEventListener("click", ()=>{
+  console.log("button pressed")
+  sendApiRequest()
+})
+
+
+//An asynchronous function to fetch data from the API.
+async function sendApiRequest(){
+  // let userInput = document.querySelector("#searchTerm").value
+  let response = await fetch(`https://api.adviceslip.com/advice`);
+  console.log(response)
+  let data = await response.json()
+  console.log(data)
+  useApiData(data)
+}
+
+
+//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
+function useApiData(data){
+  document.querySelector("#content").innerHTML += `
+<div class="card">
+  <div class="card-body">
+    ${data.slip.advice}
+  </div>
+</div>
+
+`
+ 
+}
+
+
